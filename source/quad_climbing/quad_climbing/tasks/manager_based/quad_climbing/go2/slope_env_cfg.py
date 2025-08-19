ebd42406cc674da4845aea08a656f0d31375ea12
Leo Wang <84789689+leo01110111@users.noreply.github.com>
2025-08-19 13:53:34 -0700

    joint effort penalty

--- source/quad_climbing/quad_climbing/tasks/manager_based/quad_climbing/go2/slope_env_cfg.py
+++ source/quad_climbing/quad_climbing/tasks/manager_based/quad_climbing/go2/slope_env_cfg.py
@@ -37,7 +37,7 @@ class UnitreeGo2SlopeEnvCfg(LocomotionSlopeEnvCfg):
         # rewards
         #self.rewards.feet_air_time.params["sensor_cfg"].body_names = ".*_foot"
         #self.rewards.feet_air_time.weight = 0.01
-        #self.rewards.dof_torques_l2.weight = -0.0002
+        self.rewards.dof_torques_l2.weight = -0.0002
         #self.rewards.dof_acc_l2.weight = -2.5e-7
 
         # terminations
--- source/quad_climbing/quad_climbing/tasks/manager_based/quad_climbing/slope_env_cfg.py
+++ source/quad_climbing/quad_climbing/tasks/manager_based/quad_climbing/slope_env_cfg.py
@@ -215,7 +215,7 @@ class RewardsCfg:
     )
     
     # -- penalties
-    #dof_torques_l2 = RewTerm(func=mdp.joint_torques_l2, weight=-1.0e-5)
+    dof_torques_l2 = RewTerm(func=mdp.joint_torques_l2, weight=-1.0e-5)
     #dof_acc_l2 = RewTerm(func=mdp.joint_acc_l2, weight=-2.5e-7)
     #action_rate_l2 = RewTerm(func=mdp.action_rate_l2, weight=-0.01)
     """feet_air_time = RewTerm(
