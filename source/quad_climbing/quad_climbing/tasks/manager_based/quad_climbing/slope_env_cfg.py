5181c416da6c94e02c17d544ddcec0ec49c04e2e
Leo Wang <84789689+leo01110111@users.noreply.github.com>
2025-10-26 17:58:19 -0700

    more changes

--- source/quad_climbing/quad_climbing/tasks/manager_based/quad_climbing/slope_env_cfg.py
+++ source/quad_climbing/quad_climbing/tasks/manager_based/quad_climbing/slope_env_cfg.py
@@ -81,11 +81,11 @@ class CommandsCfg:
 
     base_velocity = mdp.TrackingVelocityCommandCfg( #the config for giving commands in the mdp
         asset_name="robot", #the asset we're commanding
-        resampling_time_range=(1000.0, 1000.0), #min and max time between resampling a new command. Here we make it so that it basically doesnt happen
+        resampling_time_range=(10.0, 10.0), #min and max time between resampling a new command. Here we make it so that it basically doesnt happen
         debug_vis=True,
         heading_control_stiffness=2.0,
-        rel_standing_envs = 0.1,
-        rel_forward_envs = 0.5,
+        rel_standing_envs = 0,
+        rel_forward_envs = 1,
         #by definition, rel_random_heading_envs = 0.4
         ranges=mdp.TrackingVelocityCommandCfg.Ranges( 
             pos_x=(0, 0), pos_y=(0, 0), velocity=(1.0,1.0), ang_vel_z=(-2.0,2.0)
